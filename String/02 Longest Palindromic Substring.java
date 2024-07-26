c8e5b67df408100210c5e607fcf283863f3508cc
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-27 00:30:14 +0530

    Update 02 Longest Palindromic Substring.java

--- String/02 Longest Palindromic Substring.java
+++ String/02 Longest Palindromic Substring.java
@@ -1,45 +1,53 @@
 Problem Link : [https://leetcode.com/problems/longest-palindromic-substring/description/]
 class Solution {
-    private String s;
-    private int max_len;
-    private int st;
-    private int end;
 
-    public void getPalindromeLen(int l, int r){
-        int n = s.length();
-         while( l >= 0 && r<n) {
-            if(s.charAt(l) == s.charAt(r)){
-                l--;
-                r++;
-            }else
-                break;
-            }
-            int len = r-l-1;
-            if(len>max_len){
-                max_len = len;
-                st = l+1;
-                end = r-1;
-            }
-        }
-    
     public String longestPalindrome(String s) {
-        if(s.length() <= 1) return s;
-        this.s = s;
-        max_len = 1;
         int n = s.length();
-        st = 0; 
-        end = 0;
+        int max_len = 0;
+        int st = 0;
+        int end = 0;
 
-        //odd length;
-        for(int i = 0; i<n ; i++){
-            int l = i, r = i;
-        getPalindromeLen(l,r);
+        //odd
+        for(int i =0; i<n; i++){
+            int l=i,r=i;
+            while(l>=0 && r<n){
+                if(s.charAt(l)==s.charAt(r)){
+                    l--;
+                    r++;
+                }
+                else{
+                    break;
+                }
+                int len = r-l-1;
+                if(len>max_len){
+                    max_len = len;
+                    st = l+1;
+                    end = r-1;
+                }
+            }
         }
-        //even length;
-        for(int i = 0; i<n-1 ; i++){
-            int l = i, r = i+1;
-        getPalindromeLen(l,r);
+
+
+        //even
+         for(int i =0; i<n; i++){
+            int l=i,r=i+1;
+            while(l>=0 && r<n){
+                if(s.charAt(l)==s.charAt(r)){
+                    l--;
+                    r++;
+                }
+                else{
+                    break;
+                }
+                int len = r-l-1;
+                if(len>max_len){
+                    max_len = len;
+                    st = l+1;
+                    end = r-1;
+                }
+            }
         }
-      return s.substring(st, end + 1);
+    return s.substring(st,end+1);
     }
 }
+
